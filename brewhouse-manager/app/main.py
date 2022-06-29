9bed1c759858d1934bacd8c441f39d12f8de8f94
Scott Perkins <whatisbyandby@gmail.com>
2022-06-29 01:51:36 +0100

    Added github actions

--- brewhouse-manager/app/main.py
+++ brewhouse-manager/app/main.py
@@ -1,18 +1,14 @@
-from fastapi import FastAPI, WebSocket, Response, status, Request, Path
 import logging
 import json
-
-from app.routes.steps import step_router
-from app.routes.brewhouse import brewhouse_router
-from app.routes.pumps import pump_router
-from app.context import initialize_context
-from app.brewery_controller.brewery_controller import BreweryController
-from app.pins.pin import start_pins
-from dotenv import load_dotenv
 import os
+from dotenv import load_dotenv
+
+from app.context import initialize_context
+from app.pins.pin import create, start_pins
+from .app import create_server
 
 load_dotenv()
-app = FastAPI()
+
 
 logging.basicConfig(filename='./log/app.log', level=logging.DEBUG, filemode='w',
                     format='%(asctime)s [%(levelname)s-%(name)s] %(message)s')
@@ -28,15 +24,4 @@ config = get_config(config_file)
 start_pins()
 context = initialize_context(config=config)
 
-app.brewery_controller = BreweryController(components=context)
-
-app.include_router(step_router)
-app.include_router(brewhouse_router)
-app.include_router(pump_router)
-
-
-@app.get("/")
-async def home(request: Request):
-    logging.info(request.app)
-    logging.info("Home page")
-    return {"Hello": "World"}
+app = create_server(context=context)
