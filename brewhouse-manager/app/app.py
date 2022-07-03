dddae5e5348fc237ffd1d64356c227cce05fdeec
Scott Perkins <whatisbyandby@gmail.com>
2022-07-03 14:20:52 -0600

    Added development recipe to the make file to allow for hot reloading during development

--- brewhouse-manager/app/app.py
+++ brewhouse-manager/app/app.py
@@ -14,4 +14,8 @@ def create_server(context):
     app.include_router(brewhouse_router)
     app.include_router(pump_router)
 
+    @app.get("/")
+    def get_home():
+        return {"home_route": "Hello from the home route"}
+
     return app
